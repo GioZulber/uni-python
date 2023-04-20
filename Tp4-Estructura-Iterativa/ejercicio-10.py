ccecc6f617712a4e3a2b5df2195c76e276b5ceef
Giovanni Zulberti <87451216+GioZulber@users.noreply.github.com>
2023-04-20 10:27:55 -0300

    Update ejercicio-10.py

--- Tp4-Estructura-Iterativa/ejercicio-10.py
+++ Tp4-Estructura-Iterativa/ejercicio-10.py
@@ -8,10 +8,13 @@ num = int(input("Ingrese un número: "))
 counter = 1
 factorial = 1
 
-while num > 0 and counter <= num: 
-    print("factorial: ", factorial)
-    factorial *= counter
-    counter += 1
-print("----------------------------------------------------")
-print("Obtuvimos el siguiente resultado:", factorial)
-print("----------------------------------------------------")
+
+if num > 0:
+    while  counter <= num: 
+        factorial *= counter
+        counter += 1
+    print("----------------------------------------------------")
+    print("Obtuvimos el siguiente resultado:", factorial)
+    print("----------------------------------------------------")
+else: 
+    print("ERROR: VALOR INVALIDO.")
